52a5e4d4f8859117d75a792c0a5e51826c4b676d
lumen-novum <novablackxd@gmail.com>
2022-09-14 10:38:51 -0500

    Added Value Swapping

--- sorting.swift
+++ sorting.swift
@@ -1,24 +1,37 @@
 var words = [String]()
 var x = 1
 
-func wordtoUTF8(word: String) {
-    var dict = [Character:Int]()
-    var index = 0
-    for num in word.utf8 {
-        dict[word[index]] = Int(num)
-        index += 1
+func sort(word: String) {
+    let UTF8Key = wordtoUTF8(word: word)
+
+    while x < word.count {
+        if UTF8Key[x] < UTF8Key[x-1] {
+            
+            x -= 1
+        }
+        x += 1
     }
-    return dict
+    x = 1
 }
 
-/*
-func swap(integers:inout[Int]) {
-    let tempvar = integers[firstIndex]
-
-    integers[firstIndex] = integers[secondIndex]
-    integers[secondIndex] = tempvar
+func wordtoUTF8(word: String) -> [Int] {
+    var intArray = [Int]()
+    
+    for UTF8num in word.utf8 {
+        intArray.append(Int(UTF8num))
+    }
+    return intArray
 }
- */
+
+func swaps(value1: Any, value2: Any) {
+    var finalChange = [Any]()
+    
+    finalChange.append(value2)
+    finalChange.append(value1)
+    
+    print(finalChange)
+}
+
 while let readWord = readLine() {
     if readWord == "" {
         break
@@ -26,14 +39,6 @@ while let readWord = readLine() {
         words.append(readWord)
     }
 }
-/*
-for word in words {
-    while x < word.count {
-        
-        x += 1
-    }
-}
 
-
- */
 print(wordtoUTF8(word: words[1]))
+swaps(value1: 32, value2: 11)
